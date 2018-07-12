acb1917902a78c44a0d6c8095672745a9ebeb674
Anthony Li <li.anthony917@gmail.com>
2018-07-12 15:17:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // Auto-mine when the secret button is clicked
   autoButton.addEventListener("click", () => {
     var hash = "33"
-    while(hash.slice(0, 3) !== "000") {
+    while(hash.slice(0, 5) !== "00000") {
       var nonce = parseInt(nonceBox.value, 10)
       changeBy(1)
 
